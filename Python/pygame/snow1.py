eee71b2a19c15a0f354ceb3ab01ffeb879190987
Steven Carroll <32237935+dc-carrolls@users.noreply.github.com>
2024-10-21 08:20:53 +0100

    Feedback: snow1.py
    I should be able to see your snow flake code here... Don't worry if you haven't completed all three, but at least the first two should be completed.

---
+++ Python/pygame/snow1.py
@@ -0,0 +1,4 @@
+### SRC - Your code for the snow flakes should be here...
+### snow1.py - 3 snow flakes using variables
+### snow2.py - 50 snow flakes using 2D array
+### snow3.py - 200 snow flakes using Flake record structure in 1D array.
